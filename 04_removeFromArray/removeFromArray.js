7159a1a886d3a586766ac59c8eba295e932c3c6d
Julian <juleichen@aol.com>
2022-04-11 13:49:36 +0200

    Zwischenstand 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
+    var args = Array.from(arguments);
+    var inArray = args[0];
+    var deletUs = args.slice(1);
 
+    function deleteFilter(entry){
+        return entry in deletUs;
+    }
+
+    var outArray = inArray.filter(deleteFilter);
+    return outArray;
 };
 
 // Do not edit below this line
