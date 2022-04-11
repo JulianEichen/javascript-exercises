2566922d06f15ccf100b75d35edf083504795bac
Julian <juleichen@aol.com>
2022-04-11 12:36:07 +0200

    solution repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    const add_s = string;
+    if(num==0){
+        return '';
+    }
+    if(num<0){
+        return 'ERROR';
+    }
 
+    while (num > 1) {
+        string += add_s;
+        --num;
+    }
+    return string;
 };
 
 // Do not edit below this line
