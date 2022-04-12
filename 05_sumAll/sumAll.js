8c13da658d4617d7e6d758930384e755e5966880
Julian <juleichen@aol.com>
2022-04-12 16:50:58 +0200

    solution 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    var start = 0;
+    var end = 0;
+    if (x<0 || y<0 || typeof(x)!='number' || typeof(y)!='number'){
+        return 'ERROR';
+    }
+    if (x<y){
+        start = x;
+        end = y;
+    }else{
+        start = y;
+        end = x;
+    }
 
+    for(var i =start+1; i<=end; i++){
+        start+=i;
+    }
+    return start;
 };
 
 // Do not edit below this line
