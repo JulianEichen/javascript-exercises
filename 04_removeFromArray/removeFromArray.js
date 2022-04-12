c138340806ca0141d2e58f93c2b58aac5f452bc4
Julian <juleichen@aol.com>
2022-04-12 16:35:32 +0200

    solution 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,15 @@
 const removeFromArray = function() {
     var args = Array.from(arguments);
     var inArray = args[0];
-    var deletUs = args.slice(1);
+    var deleteUs = args.slice(1);
 
     function deleteFilter(entry){
-        return entry in deletUs;
+        var check = true;
+        if (deleteUs.indexOf(entry)>=0){
+            check = false;
+        }
+        return check;
     }
-
     var outArray = inArray.filter(deleteFilter);
     return outArray;
 };
