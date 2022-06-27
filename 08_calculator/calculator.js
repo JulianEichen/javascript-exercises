8e15abce4ecc415c22d54891fe1c90f01ee6635e
Julian <juleichen@aol.com>
2022-06-27 16:49:29 +0200

    finished08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(input_arr) {
+  return input_arr.reduce((total,number) => {
+    return total + number;
+  }, 0)
 };
 
-const multiply = function() {
+const multiply = function(input_arr) {
+  return input_arr.reduce((product,number) => {
+    return product * number;
+  },1)
 
 };
 
-const power = function() {
-	
+const power = function(bs,pwr) {
+  let prod = 1;
+	for (let i=0;i<pwr;i++){
+    prod *= bs;
+  }
+  return prod;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let prod = 1;
+  for (let i=1; i<a+1; i++){
+    prod *= i;
+  }
+  return prod;
 };
 
 // Do not edit below this line
