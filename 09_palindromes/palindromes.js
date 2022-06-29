09ef45aa7269b60802867e504384eec304bc9a95
Julian <juleichen@aol.com>
2022-06-29 11:32:15 +0200

    09 solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (text) {
 
+    text = text.toUpperCase();
+    
+    // remove punctuation
+    let text_split = text.split(".").join('');
+    text_split = text_split.split(",").join('');
+    text_split = text_split.split("!").join('');
+    text_split = text_split.split("?").join('');
+    text_split = text_split.split(" ").join('');
+
+    // create reversed copy
+    let text_rev = [...text_split];
+    text_rev = text_rev.reverse().join('');
+  
+    if (text_split ==text_rev){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
