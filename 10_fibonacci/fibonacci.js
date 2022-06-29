d26c1f05ceee80f9888277d8b6db12993b8d0f37
Julian <juleichen@aol.com>
2022-06-29 11:41:14 +0200

    10 solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num <= 0){
+        return "OOPS"
+    }
 
+    let current = 1;
+    let last = 1;
+    let dummy = 0;
+    for(let i = 2; i<num; i++){
+        dummy = current;
+        current += last;
+        last = dummy;
+    }
+    return current;
 };
 
 // Do not edit below this line
